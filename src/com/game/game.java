0633a764729382bb97c8786bc02d3e0682d52b1c
briangonzales10 <brian.gonzales10@gmail.com>
2021-06-30 16:58:45 -0500

    AB#36 created location objects and gamework object to instantiate locations

---
+++ src/com/game/game.java
@@ -0,0 +1,21 @@
+package com.game;
+
+import com.game.world.gameWorld;
+import com.game.world.location;
+
+import java.util.HashMap;
+
+public class game {
+    public static void main(String[] args) {
+        //testing for game world
+        gameWorld game = new gameWorld();
+
+        HashMap<String, location> planet1 = game.getPlanet1();
+
+        System.out.println(planet1.get("Frozen Tundra").getDescription());
+        System.out.println(planet1.get("Frozen Tundra").getDirections());
+        System.out.println(planet1.get("Crater").getDirections());
+
+    }
+}
+
---
+++ src/com/game/world/gameWorld.java
@@ -0,0 +1,33 @@
+package com.game.world;
+
+import java.util.HashMap;
+
+
+public class gameWorld {
+
+    //Field for each planet
+    private HashMap<String, location> planet1;
+
+    public gameWorld() {
+        planet1 = new HashMap<>();
+        planet1.put("Crash Site", new location(1,"Crash Site", "You see your crashed ship with obvious signs of damage. You'll need to find tools to fix them. You take a look around and see paths to the North, East and South.",
+                "Tundra", "Beach", "Mountains", null));
+        planet1.put("Frozen Tundra", new location(2, "Frozen Tundra","Temperatures quickly dropped and now you're in the middle of a frozen tundra wasteland.  You don't see much around you except a barely visible path to the west",
+                null, null, "Crash Site", "Crater"));
+        planet1.put("Crater", new location(3, "Crater", "You fall into a large, but shallow crater.  You notice some plastic crates with some Tools sticking out.  Maybe you can fix your ship with it.  You also see some vegetation starting to grow on one end of the crater",
+                null, "Frozen Tundra", "Jungle", null));
+
+    }
+
+    //Getters for planets
+    public HashMap<String, location> getPlanet1() {
+        return planet1;
+    }
+
+    @Override
+    public String toString() {
+        return "gameWorld{" +
+                "planet1=" + planet1 +
+                '}';
+    }
+}
