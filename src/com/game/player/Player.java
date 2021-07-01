935461e844aa34e4655acec5fe632e426616c478
two12dev <crob2099@gmail.com>
2021-07-01 13:59:12 -0700

    Created initial Player class and instantiated player object in game.java AB#35

---
+++ src/com/game/player/Player.java
@@ -0,0 +1,43 @@
+package com.game.player;
+
+import com.game.textparser.UserInput;
+
+import java.util.ArrayList;
+
+public class Player {
+    private String name;
+    private String currentLocation;
+    private ArrayList<String> inventory;
+
+    public Player(){
+        setName();
+
+    }
+
+    //Name Getter and Setter
+    public String getName(){
+        return name;
+    }
+
+    public void setName(){
+        name = UserInput.setPlayerName();
+        System.out.println("Welcome aboard Commander " + name + "!");
+    }
+
+    //Location
+    public String getCurrentLocation(){
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(){
+        //Need to add logic to set Location
+    }
+
+
+
+    //Inventory method will go below
+
+
+
+
+}
