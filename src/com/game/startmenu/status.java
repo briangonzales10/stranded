0b738ca9869ba4a5a62993c444b21693885dfa46
briangonzales10 <brian.gonzales10@gmail.com>
2021-07-02 15:10:00 -0500

    AB#175 created skeleton for command engine / integrated clear console code

--- src/com/game/startmenu/status.java
+++ src/com/game/startmenu/status.java
@@ -50,6 +50,35 @@ public class status {
         this.noun = noun;
     }
 
+    public static void clearConsole() throws InterruptedException {
+        /* Code Attributed to DelftStack
+            December 10, 2020
+            Use ProcessBuilder to Clear Console in Java
+            Example code
+            https://www.delftstack.com/howto/java/java-clear-console/
+         */
+
+        Thread.sleep(2000);
+
+        try{
+            String operatingSystem = System.getProperty("os.name"); //Check the current operating system
+
+            if(operatingSystem.contains("Windows")){
+                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "cls");
+                Process startProcess = pb.inheritIO().start();
+
+                startProcess.waitFor();
+            } else {
+                ProcessBuilder pb = new ProcessBuilder("clear");
+                Process startProcess = pb.inheritIO().start();
+
+                startProcess.waitFor();
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
 //    public static String getStatus() {
 //        return name + description + commands;
 //    }
