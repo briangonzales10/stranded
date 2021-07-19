be12e9d29770d4d3ba43c5ed3420bb4319ac0042
Daniel Lasche <dlasche@gmail.com>
2021-07-19 11:21:35 -0700

    AB#361 Current location updates when you hit send button

--- strandedfx/src/sample/Controller.java
+++ strandedfx/src/sample/Controller.java
@@ -1,20 +1,27 @@
 package sample;
 
+import com.game.world.GameWorld;
+import com.game.world.Location;
 import com.sun.media.jfxmediaimpl.platform.Platform;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Objects;
 import javafx.event.ActionEvent;
 import com.game.Main;
 
 import javax.swing.*;
+
+import static com.game.world.GameWorld.currentLocation;
 
 
 public class Controller {
@@ -24,25 +31,47 @@ public class Controller {
     private Parent root;
     public TextField playerName;
     public Label displayAstroName;
+    @FXML
+    public Label fxCurrentLocation;
+    @FXML
+    public Label fxDescription;
+    @FXML
+    Button sendCommand;
 
+    GameWorld ourGame = new GameWorld();
+    HashMap<String, Location> planet1 = ourGame.getPlanet1();
 
-
+    public Controller() throws IOException {
+    }
 
 
     public void switchToStartGame(ActionEvent event) throws IOException, InterruptedException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("playScreen.fxml")));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        stage.setTitle("Stranded Play Screen");
+        stage.setTitle(currentLocation);
 
         submitName(event);
         scene = new Scene(root);
         stage.setScene(scene);
+        //Label topCurrentLocation = new Label();
+        //topCurrentLocation.setText(currentLocation);
+        //System.out.println(topCurrentLocation);
+
+        fxCurrentLocation.setText(currentLocation);
         stage.show();
+        //fxCurrentLocation.setText(currentLocation);
+        //System.out.println(fxCurrentLocation.toString());
 
     }
 
     public void startGameToFX() throws IOException, InterruptedException {
         Main.gameToFX();
+    }
+
+    public void setSendCommand(){
+        fxCurrentLocation.setText(currentLocation);
+        System.out.println(planet1.get("description"));
+
     }
 
 
@@ -61,8 +90,7 @@ public class Controller {
     return name;
     }
 
-
-
+    //Method that creates a new stage/scene that displays the travel ascii
 
 
 }
